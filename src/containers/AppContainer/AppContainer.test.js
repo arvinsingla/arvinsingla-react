22189ecaa16541d4fd4ecda18132dc064aa6e300
Arvin Singla <arvin.singla@gmail.com>
2018-01-21 23:43:09 -0500

    Updating test to pass and adding circleci config file.

--- src/containers/AppContainer/AppContainer.test.js
+++ src/containers/AppContainer/AppContainer.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { MemoryRouter } from 'react-router'
+import AppContainer from './AppContainer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <MemoryRouter>
+      <AppContainer />
+    </MemoryRouter>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
